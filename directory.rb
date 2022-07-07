fcd49b8f81d5a90bdf5a0edc5adff3a2ba8a4027
Zac <zac@zacmoss.co.uk>
2022-07-07 17:55:10 +0100

    added interactivity

--- directory.rb
+++ directory.rb
@@ -25,6 +25,20 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return"
+  students = []
+  name = gets.chomp
+  while !name.empty?
+    students << {name: name, cohort: :novemeber}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
