d8f116b2d737b936b03c76805c3533026d71f323
Zac <zac@zacmoss.co.uk>
2022-07-07 20:56:27 +0100

    added load function

--- directory.rb
+++ directory.rb
@@ -1,21 +1,20 @@
 require 'date'
 @months = (1..12).map {|m| Date::MONTHNAMES[m].downcase.to_sym}
+@students = []
 
-# @students = []
-
-@students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :october},
-  {name: "The Wicked Witch of the West", cohort: :september},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :january},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# @students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :october},
+#   {name: "The Wicked Witch of the West", cohort: :september},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :january},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
+# ]
 
 def print_header
   if @students.empty?
@@ -48,29 +47,33 @@ def print_footer
 end
 
 def input_students
-  months_hash = @months.each_with_object({}) { |month, hash| hash[@months.index(month) + 1] = month}
+  @months_hash = @months.each_with_object({}) { |month, hash| hash[@months.index(month) + 1] = month}
   puts "Please enter the names of the students"
   puts "To finish, just hit return"
   name = gets.chomp
   while !name.empty?
-    cohort = ""
-    while true
-      puts "Enter a cohort or hit return for the current month"
-      cohort = gets.chomp
-      cohort = (cohort.empty?) ? months_hash[Date.today.strftime("%m").to_i] : cohort.downcase.to_sym
-      break if months_hash.values.include? cohort
-      puts "You did not enter a valid cohort"
-    end
+    cohort = cohort_loop
     @students << {name: name, cohort: cohort}
     puts "Now we have #{@students.count} student#{"s" if @students.count != 1}"
     name = gets.chomp
   end
 end
 
+def cohort_loop
+  loop {
+      puts "Enter a cohort or hit return for the current month"
+      cohort = gets.chomp
+      cohort = (cohort.empty?) ? @months_hash[Date.today.strftime("%m").to_i] : cohort.downcase.to_sym
+      return cohort if @months_hash.values.include? cohort
+      puts "You did not enter a valid cohort"
+  }
+end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -84,7 +87,8 @@ def process(selection)
   case selection
     when "1" then input_students
     when "2" then show_students
-    when "3" then puts save_students
+    when "3" then save_students
+    when "4" then load_students
     when "9" then Exit
     else puts "I don't know what you meant, try again"
   end
@@ -92,15 +96,24 @@ end
 
 def interactive_menu
   # @students = []
-  loop do
+  loop {
     print_menu
     process(gets.chomp)
-  end
+  }
 end
 
 def save_students
   file = File.open("students.csv", "w")
   @students.each { |student| file.puts "#{student[:name]}, #{student[:cohort]}" }
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each { |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  }
   file.close
 end
 
